4dedfbd8af89fd73f356a73a830c9ef7d4883896
olafurw <olafurw@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-07-01 20:36:38 +0000

    Changed the size of the loops from 300k to 20k
    git-svn-id: https://svn.php.net/repository/php/php-benchmarks/trunk@283297 c90b9560-bf6c-de11-be94-00142212c4b1

--- benchcli/microtests/test_array-iterator.php
+++ benchcli/microtests/test_array-iterator.php
@@ -1,26 +1,23 @@
 <?php
-$size = 300000;
+$size = 20000;
 
 // create array
-
 $testArray = array();
 
-for ($i=0; $i<$size; $i++) {
-	
+for ($i=0; $i<$size; $i++)
+{	
 	$testArray[$i] = rand(0,9);
 }
 
 $testObject = new ArrayObject($testArray);
 $iterator = $testObject->getIterator();
 
-while ($iterator->valid()) {
-	
-	if ($iterator->key()%2 == 0) {
-			
-		$testObject[$iterator->current()] = 'G';
+while ($iterator->valid())
+{
+	if ($iterator->key() % 2 == 0)
+    {
+        $testObject[$iterator->current()] = 'G';
 	}
 
 	$iterator->next();
 }
-
-?>
--- benchcli/microtests/test_array-loop.php
+++ benchcli/microtests/test_array-loop.php
@@ -1,25 +1,22 @@
 <?php
-$size = 300000;
-// create array
+$size = 20000;
 
+// create array
 $testArray = array();
 
-for ($i=0; $i<$size; $i++) {
-	
+for ($i=0; $i<$size; $i++)
+{	
 	$testArray[$i] = rand(0,9);
 }
 
 // loop
-
-for ($i=0; $i<$size; $i++) {
-	
-	if (isset($testArray[$i])) {
-	
-		if ($i%2 == 0) {
-			
+for ($i=0; $i<$size; $i++)
+{	
+	if (isset($testArray[$i]))
+    {
+		if ($i%2 == 0)
+        {
 			$testArray[$i] = 'G';
 		}
 	}
 }
-
-?>
